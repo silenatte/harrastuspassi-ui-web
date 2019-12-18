f27099b98e1d7621b0e106dde9359de1c73fb00f
Eetu Kallio <eetu.kallio@haltu.fi>
2019-12-18 12:05:08 +0200

    Fix hobby deletion button having too wide of an area

--- src/components/blocks/HobbyListItem.jsx
+++ src/components/blocks/HobbyListItem.jsx
@@ -8,6 +8,7 @@ import {
   ListItemSecondaryAction,
   IconButton
 } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles({
@@ -21,15 +22,24 @@ const useStyles = makeStyles({
 });
 
 const HobbyListItem = props => {
-  const { deleteHandler, imageUrl, name, secondaryText, showControls } = props;
+  const {
+    deleteHandler,
+    imageUrl,
+    name,
+    secondaryText,
+    showControls,
+    id
+  } = props;
   const classes = useStyles();
   return (
     <div>
       <ListItem disableGutters={true}>
-        <ListItemIcon>
-          <img className={classes.hobbyListImage} alt={name} src={imageUrl} />
-        </ListItemIcon>
-        <ListItemText primary={name} secondary={secondaryText} />
+        <Link key={id} to={`/hobbies/edit/${id}`}>
+          <ListItemIcon>
+            <img className={classes.hobbyListImage} alt={name} src={imageUrl} />
+          </ListItemIcon>
+          <ListItemText primary={name} secondary={secondaryText} />
+        </Link>
         {showControls ? (
           <ListItemSecondaryAction>
             <IconButton edge="end" aria-label="delete" onClick={deleteHandler}>
